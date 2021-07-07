05d596c74add4c65f71c3b7b1c25c302d4417620
Atul khanal <atulkhanal37@gmail.com>
2021-07-07 19:16:22 +0545

    atul

---
+++ oddheavyornot.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+int oddheavy(int num[], int);
+int main()
+{
+    int n;
+    printf("enter the number of elements in array ");
+    scanf("%d\n", &n);
+    int num[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &num[i]);
+    printf("%d\n", oddheavy(num, n));
+    return 0;
+}
+int oddheavy(int num[], int size)
+
+{
+    int e=0, o=0, z, var, even[size], odd[size];
+    for (int d = 0; d < size; d++)
+    {
+        for (int c = d + 1; c <= size; c++)
+        {
+            if (num[c] < num[d])
+            {
+                var = num[c];
+                num[c] = num[d];
+                num[d] = var;
+            }
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+        if (num[i] % 2 == 0)
+        {
+            even[e] = num[i];
+            e+=1;
+        }
+        else{
+            odd[o] = num[i];
+            o+=1;
+        }
+    }
+    for (int i = 0; i <(e-1); i++)
+    {
+        for (int j = 0; j <(o-1); j++)
+        {
+            if ((even[i] == 0) || (odd[j] == 0))
+                continue;
+            else if (even[i] < odd[j])
+            {
+                z = 1;
+            }
+            else
+            {
+                z = 0;
+                break;
+            }
+        }
+    }
+    return z;
+}
